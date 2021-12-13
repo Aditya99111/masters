cda627fde2b1502b948cc43d4aa6b906f7c10096
john Cervantes <johncamilocervantes@gmail.com>
2021-12-13 09:08:54 -0600

    fixed posts bug

--- components/blogs/Blog.tsx
+++ components/blogs/Blog.tsx
@@ -1,31 +1,10 @@
 import React from "react";
 import blogstyles from "../../styles/Blogspage.module.css";
 
-const BLOG_URL = "https://guild-masters-den.herokuapp.com";
-const CONTENT_API_KEY = "bdd359f52eb21b10b4f5d95ae3";
-
-type Post = {};
-async function getPosts() {
-  // curl "https://demo.ghost.io/ghost/api/v3/content/posts/?key=22444f78447824223cefc48062"
-  const res = await fetch(
-    `${BLOG_URL}/ghost/api/v3/content/posts/?key=${CONTENT_API_KEY}`
-  ).then((res) => res.json());
-
-  const titles = res.posts.map((post) => post.title);
-
-  console.log(titles);
-  console.log("hello");
-  return titles;
-}
-export const getStaticProps = async ({ params }) => {
-  const posts = await getPosts();
-  return {
-    props: { posts },
-  };
-};
-
 const Blog: React.FC<{ posts: string[] }> = (props) => {
   const { posts } = props;
+
+  console.log(posts);
   return (
     <div>
       <section className={blogstyles.blogspage} id="blogs">
--- pages/stories.tsx
+++ pages/stories.tsx
@@ -1,15 +1,45 @@
 import React from "react";
 import Header from "../components/home/header/Header";
 import Blog from "../components/blogs/Blog";
-const stories = () => {
+const stories = (props) => {
+  console.log(props);
   return (
     <div>
       <>
-      <Header />
-      <Blog />
+        <Header />
+        <Blog posts={props.posts} />
       </>
     </div>
   );
+};
+
+const BLOG_URL = "https://demo.ghost.io";
+const CONTENT_API_KEY = "22444f78447824223cefc48062";
+
+type post = {};
+async function getPosts() {
+  // curl "https://demo.ghost.io/ghost/api/v3/content/posts/?key=22444f78447824223cefc48062"
+  const res = await fetch(
+    `${BLOG_URL}/ghost/api/v3/content/posts/?key=${CONTENT_API_KEY}`
+  ).then((res) => res.json());
+
+  const titles = res.posts.map((post) => post.title);
+
+  return titles;
+}
+
+export const getStaticProps = async ({ params }) => {
+  const res = await fetch(
+    `${BLOG_URL}/ghost/api/v3/content/posts/?key=${CONTENT_API_KEY}`
+  ).then((res) => res.json());
+
+  const titles = res.posts.map((post) => post.title);
+  console.log(titles);
+
+  const posts = await getPosts();
+  return {
+    props: { posts },
+  };
 };
 
 export default stories;
