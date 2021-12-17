74dd5a77a5f96eb42da597beadd120f031216cf9
Aditya99111 <adityamittal471@gmail.com>
2021-12-17 21:15:54 +0530

    test

--- components/home/contact/Contact.js
+++ components/home/contact/Contact.js
@@ -14,10 +14,10 @@ const Contact = () => {
 
                 <form action="https://sheetdb.io/api/v1/mh5pwfr9b4xep" method="POST" id="form">
                     <h3 data-aos="zoom-in">tell us something!</h3>
-                    <input name="data[name]" type="text" placeholder="Your name" className={contactstyles.box} />
-                    <input name="data[email]" type="email" placeholder="Your email" className={contactstyles.box} />
-                    <input name="data[subject]" type="text" placeholder="Subject :" className={contactstyles.box} />
-                    <textarea name="data[message]" placeholder="Your message" className={contactstyles.box} cols="30" rows="10"></textarea>
+                    <input name="data[name]" type="text" placeholder="Your name" className={contactstyles.box} required />
+                    <input name="data[email]" type="email" placeholder="Your email" className={contactstyles.box} required/>
+                    <input name="data[subject]" type="text" placeholder="Subject :" className={contactstyles.box} required/>
+                    <textarea name="data[message]" placeholder="Your message" className={contactstyles.box} cols="30" rows="10" required></textarea>
                     <input data-aos="zoom-in" type="submit" value="send message" className={contactstyles.btn} />
                 </form>
 
